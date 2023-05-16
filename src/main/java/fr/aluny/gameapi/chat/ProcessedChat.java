7aad36f2e0e72ebfbab57376a1e96dc5e9a66788
D0gma_ <d0gma@d0gma.fr>
2023-05-16 16:57:52 +0200

    Change ProcessedChat to use Component

--- src/main/java/fr/aluny/gameapi/chat/ProcessedChat.java
+++ src/main/java/fr/aluny/gameapi/chat/ProcessedChat.java
@@ -4,34 +4,37 @@ import fr.aluny.gameapi.player.GamePlayer;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.function.Function;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
 
 public class ProcessedChat {
 
-    private String messageContent;
+    private TextComponent messageContent;
 
-    private final GamePlayer        sender;
-    private final Map<UUID, String> contentForPlayer = new HashMap<>();
+    private final GamePlayer           sender;
+    private final Map<UUID, Component> contentForPlayer = new HashMap<>();
 
     private boolean cancelled = false;
 
     public ProcessedChat(GamePlayer sender, String messageContent) {
         this.sender = sender;
-        this.messageContent = messageContent;
+        this.messageContent = Component.text(messageContent);
     }
 
-    public String getMessageContent() {
+    public TextComponent getMessageContent() {
         return this.messageContent;
     }
 
-    public void setMessageContent(String messageContent) {
-        this.messageContent = messageContent;
+    public void changeMessageContent(Function<TextComponent, TextComponent> transformer) {
+        this.messageContent = transformer.apply(this.messageContent);
     }
 
-    public void setContentForPlayer(UUID uuid, String message) {
+    public void setContentForPlayer(UUID uuid, Component message) {
         this.contentForPlayer.put(uuid, message);
     }
 
-    public String getContentForPlayer(UUID uuid) {
+    public Component getContentForPlayer(UUID uuid) {
         return this.contentForPlayer.getOrDefault(uuid, this.messageContent);
     }
 
