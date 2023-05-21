6a1a39e184568a14759a4c5c9144cbbaaac687ee
D0gma_ <d0gma@d0gma.fr>
2023-05-21 15:13:15 +0200

    Implement player account cache

--- src/main/java/fr/aluny/gameapi/player/PlayerAccountService.java
+++ src/main/java/fr/aluny/gameapi/player/PlayerAccountService.java
@@ -8,8 +8,12 @@ public interface PlayerAccountService extends Service {
 
     PlayerAccount getPlayerAccount(OfflineGamePlayer gamePlayer);
 
+    PlayerAccount getPlayerAccount(OfflineGamePlayer gamePlayer, boolean forceRefresh);
+
     Optional<PlayerAccount> getPlayerAccount(UUID uuid);
 
     Optional<PlayerAccount> getPlayerAccountByName(String name);
+
+    boolean isOnline(UUID uuid);
 
 }
