c6e169618a9fc9802aacd7834022026d3fe3e330
D0gma_ <d0gma@d0gma.fr>
2023-06-12 18:58:28 +0200

    implement Vanish setting

--- src/main/java/fr/aluny/gameapi/scoreboard/team/ScoreboardTeamService.java
+++ src/main/java/fr/aluny/gameapi/scoreboard/team/ScoreboardTeamService.java
@@ -2,12 +2,13 @@ package fr.aluny.gameapi.scoreboard.team;
 
 import fr.aluny.gameapi.service.Service;
 import java.util.Optional;
+import net.kyori.adventure.text.Component;
 
 public interface ScoreboardTeamService extends Service {
 
     Optional<ScoreboardTeam> getScoreboardTeam(String name);
 
-    ScoreboardTeam registerScoreboardTeam(String name, String prefix);
+    ScoreboardTeam registerScoreboardTeam(String name, Component prefix);
 
     void deleteScoreboardTeam(ScoreboardTeam scoreboardTeam);
 
