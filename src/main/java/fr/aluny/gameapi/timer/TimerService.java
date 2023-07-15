e35b14b813050e76d7189b20d24fe3f8c31c3583
D0gma_ <d0gma@d0gma.fr>
2023-07-15 15:09:11 +0200

    add Timer consumer method

--- src/main/java/fr/aluny/gameapi/timer/TimerService.java
+++ src/main/java/fr/aluny/gameapi/timer/TimerService.java
@@ -4,6 +4,7 @@ import fr.aluny.gameapi.service.Service;
 import fr.aluny.gameapi.value.TimeValue;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 import java.util.function.LongConsumer;
 
 public interface TimerService extends Service {
@@ -15,6 +16,10 @@ public interface TimerService extends Service {
     Timer registerTimer(String key, Long delay, Long step, Long stop, TimeUnit timeUnit, LongConsumer runOnTick, LongConsumer runOnEnd);
 
     Timer registerTimerFromTimeValue(String key, TimeValue step, TimeValue stop, LongConsumer runOnTick, LongConsumer runOnEnd);
+
+    Timer registerTimer(String key, Long delay, Long step, Long stop, TimeUnit timeUnit, Consumer<Timer> runOnTick, Consumer<Timer> runOnEnd);
+
+    Timer registerTimerFromTimeValue(String key, TimeValue step, TimeValue stop, Consumer<Timer> runOnTick, Consumer<Timer> runOnEnd);
 
     Optional<Timer> getTimer(String key);
 
