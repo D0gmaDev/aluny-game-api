7e95aa9ec4890698f56590790372b75abe54dd88
D0gma_ <d0gma@d0gma.fr>
2023-07-24 21:52:54 +0200

    use actual chat

--- src/main/java/fr/aluny/gameapi/chat/ChatPreProcessor.java
+++ src/main/java/fr/aluny/gameapi/chat/ChatPreProcessor.java
@@ -3,8 +3,8 @@ package fr.aluny.gameapi.chat;
 import java.util.function.Consumer;
 
 /**
- * A PreProcessor is called asynchronously when a {@link org.bukkit.event.player.AsyncPlayerChatEvent chat event} is triggered, and can perform mutable operations on
- * the {@link ProcessedChat} such as cancel it or format it (whether globally or individually)
+ * A PreProcessor is called asynchronously when a {@link io.papermc.paper.event.player.AsyncChatEvent chat event} is triggered, and can perform mutable operations on
+ * the {@link ProcessedChat} such as cancel it or modify the content.
  *
  * @see ProcessedChat
  */
