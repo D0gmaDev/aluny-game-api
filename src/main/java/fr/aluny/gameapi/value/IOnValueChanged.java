214f77e34e2fd4b72ce89ae29309e90df71b1aef
D0gma_ <d0gma@d0gma.fr>
2023-07-29 14:10:21 +0200

    fix and sanity checks

--- src/main/java/fr/aluny/gameapi/value/IOnValueChanged.java
+++ src/main/java/fr/aluny/gameapi/value/IOnValueChanged.java
@@ -1,5 +1,6 @@
 package fr.aluny.gameapi.value;
 
+@FunctionalInterface
 public interface IOnValueChanged<T> {
 
     void valueChanged(T oldValue, T newValue);
--- src/main/java/fr/aluny/gameapi/world/anchor/Anchor.java
+++ src/main/java/fr/aluny/gameapi/world/anchor/Anchor.java
@@ -1,8 +1,15 @@
 package fr.aluny.gameapi.world.anchor;
 
+import java.util.Objects;
 import org.bukkit.Location;
 
 public record Anchor(String key, String[] args, Location location) {
+
+    public Anchor {
+        Objects.requireNonNull(key);
+        Objects.requireNonNull(args);
+        Objects.requireNonNull(location);
+    }
 
     public Location blockLocation() {
         return location.getBlock().getLocation();
